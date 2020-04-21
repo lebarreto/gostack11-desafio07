c6b7f1fce7f5850e1b896b65610086376455d465
leticialmb <leticiabarreto196@gmail.com>
2020-04-21 12:33:38 -0400

    desafio 07

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
-import filesize from 'filesize';
-
 import Header from '../../components/Header';
 import FileList from '../../components/FileList';
 import Upload from '../../components/Upload';
@@ -23,19 +21,34 @@ const Import: React.FC = () => {
   const history = useHistory();
 
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
+    const data = new FormData();
 
-    // TODO
+    uploadedFiles.map(async files => {
+      for (files of uploadedFiles) {
+        data.append('file', files.file);
 
-    try {
-      // await api.post('/transactions/import', data);
-    } catch (err) {
-      // console.log(err.response.error);
-    }
+        try {
+          await api.post('/transactions/import', data);
+        } catch (err) {
+          console.log(err.response.error);
+        }
+
+        data.delete('file');
+      }
+    });
+
+    history.push('/');
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    files.forEach(file => {
+      const newFile: FileProps = {
+        file,
+        name: file.name,
+        readableSize: String(file.size)
+      }
+      setUploadedFiles([...uploadedFiles, newFile]);
+    });
   }
 
   return (
